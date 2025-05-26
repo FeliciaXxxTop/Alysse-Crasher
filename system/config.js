aeea4578674b87847a26db4885dca09bc21b332a
FeliciaXxxTop <feliciaxxxontop@gmail.com>
2025-05-26 23:51:27 +0700

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ RIKZZ BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: ryzzxd           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 1.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6285282562644"] 
+global.namabot = 'Alysse-Project'
+//======================
+global.mess = { 
+owner: 'waduhh!, lu bukan owner gw bg',
+premium: 'anda bukan user premium',
+succes: 'done bang'
+}
+//======================
